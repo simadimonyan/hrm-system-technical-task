0e3899120c7ec3f726ad90ebc5b6ff847382bf97
Dimitri Simonyan <simadimonyan@gmail.com>
2025-07-31 01:43:31 +0300

    + updated properties & build.gradle.kts

--- company-service/build.gradle.kts
+++ company-service/build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // db
+    runtimeOnly("org.postgresql:postgresql")
 }
 
 dependencyManagement {
--- employee-service/build.gradle.kts
+++ employee-service/build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // db
+    runtimeOnly("org.postgresql:postgresql")
 }
 
 dependencyManagement {
