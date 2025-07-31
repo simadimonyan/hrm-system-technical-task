329e94785009693bbb985451cdddb40360df1a6c
Dimitri Simonyan <simadimonyan@gmail.com>
2025-07-31 14:43:20 +0300

    company-service: + jpa

---
+++ company-service/src/main/java/company/web/dto/EmployeeResponse.java
@@ -0,0 +1,9 @@
+package company.web.dto;
+
+public class EmployeeResponse {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String companyId;
+}
