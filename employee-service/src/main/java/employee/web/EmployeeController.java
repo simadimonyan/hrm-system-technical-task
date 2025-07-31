8ffca0cc56a94ccd46215134c123b50499651c82
Dimitri Simonyan <simadimonyan@gmail.com>
2025-07-31 14:42:49 +0300

    company-service: + jpa

---
+++ employee-service/src/main/java/employee/web/EmployeeController.java
@@ -0,0 +1,21 @@
+package employee.web;
+
+import employee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+
+
+}
