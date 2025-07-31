a60afc9192bfecd9fd30a2ffd027f4bdc19c300f
Dimitri Simonyan <simadimonyan@gmail.com>
2025-07-31 21:33:24 +0300

    company-service: + updated build & discovery client

--- company-service/src/main/java/company/App.java
+++ company-service/src/main/java/company/App.java
@@ -2,8 +2,10 @@ package company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class App {
 
     public static void main(String[] args) {
