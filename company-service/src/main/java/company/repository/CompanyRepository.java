a8c7a600d78665c6ba868e51bcb3e3802ac07efa
Dimitri Simonyan <simadimonyan@gmail.com>
2025-07-31 14:37:07 +0300

    company-service: + jpa

--- company-service/src/main/java/company/repository/CompanyRepository.java
+++ company-service/src/main/java/company/repository/CompanyRepository.java
@@ -2,5 +2,7 @@ package company.repository;
 
 import company.repository.entities.CompanyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {}
